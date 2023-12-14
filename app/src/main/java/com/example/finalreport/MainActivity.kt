a763761614d3e4cd1e4ef8dcf798aa392dc8c96b
11010106 <11010106@gm.chihll.edu.tw>
2023-12-14 13:43:40 +0800

    LocationManager設定 生命週期開始 生命週期結束

--- app/src/main/java/com/example/finalreport/MainActivity.kt
+++ app/src/main/java/com/example/finalreport/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.finalreport
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() , LocationListener {
+    //LocationManager設定
+    private lateinit var locationManager: LocationManager
+    private  var hasGPS:Boolean=false
+    private  var hasNetwork:Boolean=false
+    //class MainActivity
+
+    //生命週期開始
+    override fun onResume() {
+        super.onResume()
     }
+    //生命週期結束
+    override fun onPause() {
+        super.onPause()
+        locationManager.removeUpdates(this)
+    }
+    override fun onLocationChanged(p0: Location) {
+        TODO("Not yet implemented")
+    }
+
 }
