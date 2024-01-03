fd8a4af84fb488b02f7985f46ba81d01e68a5f4e
jyeling <tso.jyeling@gmail.com>
2024-01-03 10:54:16 +0800

    105 tv滾動

--- app/src/main/java/com/example/finalreport/theater.kt
+++ app/src/main/java/com/example/finalreport/theater.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import android.location.Address
 import android.location.Geocoder
+import android.text.method.ScrollingMovementMethod
 import java.io.IOException
 
 class theater : AppCompatActivity() {
@@ -24,73 +25,75 @@ class theater : AppCompatActivity() {
     private val targetUrl = "https://data.ntpc.gov.tw/api/datasets/61C99F42-8A90-4ADC-9C40-BA9E0EA097AA/json?page=0&size=1000"
     private var getString = ""
     private var getString2 = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         myBind = ActivityTheaterBinding.inflate(layoutInflater)
         setContentView(myBind.root)
 //        setContentView(R.layout.activity_theater)
+         myBind.textView.movementMethod = ScrollingMovementMethod.getInstance() // 滾動文字
 
         myBind.btnUpdate.setOnClickListener {
-            //設定 OKhttp (開始 Download 資料
-            val client = OkHttpClient.Builder().build()
-            val request = Request.Builder().url(targetUrl).build()
-            //work Thread (Coroutines) , 資料分析
-            GlobalScope.launch {
-                // runBlocking
-                try {
-                    val response = client.newCall(request).execute()
-                    response.body?.use {
-                        getString = it.string()   // 已取得 JSON
-                        Log.d("myTag", "$getString")
+            fetchDataAndDisplayResult()
+        }
+    }
+    private fun fetchDataAndDisplayResult() {
+        //設定 OKhttp (開始 Download 資料
+        val client = OkHttpClient.Builder().build()
+        val request = Request.Builder().url(targetUrl).build()
 
-                        val jsonData = JSONArray(getString)
-                        val resultStringBuilder = StringBuilder()
+        //work Thread (Coroutines) , 資料分析
+        GlobalScope.launch {
+            // runBlocking
+            try {
+                val response = client.newCall(request).execute()
+                response.body?.use {
+                    getString = it.string()   // 已取得 JSON
+                    Log.d("myTag", "$getString")
 
-                        for (i in 0 until jsonData.length()) {
-                            val jO = jsonData.getJSONObject(i)
-                            val jname = jO.getString("name")
-                            val jaddress = jO.getString("address")
-                            val jphone = jO.getString("tel")
+                    val jsonData = JSONArray(getString)
+                    val resultStringBuilder = StringBuilder()
 
+                    for (i in 0 until jsonData.length()) {
+                        val jO = jsonData.getJSONObject(i)
+                        val jname = jO.getString("name")
+                        val jaddress = jO.getString("address")
+                        val jphone = jO.getString("tel")
 
+                        // 地址轉經緯
+                        val address = jaddress
+                        val latLng = getAddressLatLng(this@theater, address)
 
-                            // 地址轉經緯
-                            val address = jaddress
-                            val latLng = getAddressLatLng(this@theater, address)
-
-                            if (latLng != null) {
-                                val (latitude, longitude) = latLng
-                                withContext(Dispatchers.Main) {
-                                    //myBind.textView3.text = "Latitude: $latitude, Longitude: $longitude"
-                                    Log.d("myTag", "Address: $address, Latitude: $latitude, Longitude: $longitude")
-
-                                    getString2="\nLatitude: $latitude, Longitude: $longitude"
-                                }
-                            } else {
-                                withContext(Dispatchers.Main) {
-                                    //myBind.textView3.text = "Unable to convert address to LatLng."
-                                    Log.d("myTag", "Unable to convert address to LatLng.")
-                                }
-                            }
-
-                            val theaterInfo = "电影院 : $jname\n" +
-                                    "Telephone :$jphone\n" +
-                                    "address : $jaddress\n"+
-                                    "經緯度:\n$getString2"
-
-                            resultStringBuilder.append(theaterInfo)
-                            //
+                        if (latLng != null) {
+                            val (latitude, longitude) = latLng
                             withContext(Dispatchers.Main) {
-                                myBind.textView.text = resultStringBuilder.toString()
-                                Log.d("myTag", "$theaterInfo ")
+                                Log.d("myTag", "Address: $address, Latitude: $latitude, Longitude: $longitude")
+
+                                getString2 = "Latitude: $latitude \n, Longitude: $longitude"
+                            }
+                        } else {
+                            withContext(Dispatchers.Main) {
+                                Log.d("myTag", "Unable to convert address to LatLng.")
                             }
                         }
+
+                        val theaterInfo = "电影院 : $jname\n" +
+                                "Telephone :$jphone\n" +
+                                "address : $jaddress\n" +
+                                "經緯度:\n $getString2"
+
+                        resultStringBuilder.append(theaterInfo)
+                        //
+                        withContext(Dispatchers.Main) {
+                            myBind.textView.text = resultStringBuilder.toString()
+                            Log.d("myTag", "$theaterInfo ")
+                        }
                     }
-                } catch (e: IOException) {
-                    Log.e("myTag", "Error: ${e.toString()}")
-                } catch (e: JSONException) {
-                    Log.e("myTag", "Error: ${e.toString()}")
                 }
+            } catch (e: IOException) {
+                Log.e("myTag", "Error: ${e.toString()}")
+            } catch (e: JSONException) {
+                Log.e("myTag", "Error: ${e.toString()}")
             }
         }
     }
