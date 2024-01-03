367213dee542f23476767db6d508d503fc59ecd3
jyeling <tso.jyeling@gmail.com>
2024-01-03 10:40:52 +0800

    105 遍歷JSON輸出經緯成功

--- app/src/main/java/com/example/finalreport/pageIntent.kt
+++ app/src/main/java/com/example/finalreport/pageIntent.kt
@@ -3,47 +3,73 @@ package com.example.finalreport
 import com.example.finalreport.databinding.ActivityPageIntentBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Context
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import android.location.Address
 import android.location.Geocoder
 import java.io.IOException
 import android.util.Log
 
 
-
 class pageIntent : AppCompatActivity() {
     private lateinit var myBind: ActivityPageIntentBinding
+    private val targetUrl = "https://data.ntpc.gov.tw/api/datasets/61C99F42-8A90-4ADC-9C40-BA9E0EA097AA/json?page=0&size=1000"
+    private var getString = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         myBind = ActivityPageIntentBinding.inflate(layoutInflater)
         setContentView(myBind.root)
 
-        // 使用範例
-        val address = "台北市信義區市府路1號"
-        val latLng = getAddressLatLng(this, address)
 
-        if (latLng != null) {
-            val (latitude, longitude) = latLng
-            myBind.textView2.text = "Latitude: $latitude, Longitude: $longitude"
-            Log.d("myTag", "Address: $address, Latitude: $latitude, Longitude: $longitude")
-        } else {
-            myBind.textView2.text = "Unable to convert address to LatLng."
-            Log.d("myTag", "Unable to convert address to LatLng.")
-        }
-    }
+            //設定 OKhttp (開始 Download 資料
+            val client = OkHttpClient.Builder().build()
+            val request = Request.Builder().url(targetUrl).build()
+            //work Thread (Coroutines) , 資料分析
+            GlobalScope.launch {
+                // runBlocking
+                try {
+                    val response = client.newCall(request).execute()
+                    response.body?.use {
+                        getString = it.string()   // 已取得 JSON
+                        Log.d("myTag", "$getString")
 
-    private fun getAddressLatLng(context: Context, addressString: String): Pair<Double, Double>? {
-        val geocoder = Geocoder(context)
-        try {
-            val addresses: List<Address> = geocoder.getFromLocationName(addressString, 1) as List<Address>
-            if (addresses.isNotEmpty()) {
-                val latitude = addresses[0].latitude
-                val longitude = addresses[0].longitude
-                return Pair(latitude, longitude)
+                        val jsonData = JSONArray(getString)
+                        val resultStringBuilder = StringBuilder()
+
+                        for (i in 0 until jsonData.length()) {
+                            val jO = jsonData.getJSONObject(i)
+                            val jname = jO.getString("name")
+                            val jaddress = jO.getString("address")
+                            val jphone = jO.getString("tel")
+
+                            val theaterInfo = "电影院 : $jname\n" +
+                                    "Telephone :$jphone\n" +
+                                    "address : $jaddress\n\n"
+
+                            resultStringBuilder.append(theaterInfo)
+
+                            // 如果需要将每个地址转换为经纬度，可以在这里添加相应的逻辑
+                        }
+
+                        withContext(Dispatchers.Main) {
+                            myBind.textView2.text = resultStringBuilder.toString()
+                            Log.d("myTag", "Output all data")
+                        }
+                    }
+                } catch (e: IOException) {
+                    Log.e("myTag", "Error: ${e.toString()}")
+                } catch (e: JSONException) {
+                    Log.e("myTag", "Error: ${e.toString()}")
+                }
             }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return null
     }
 }
--- app/src/main/java/com/example/finalreport/theater.kt
+++ app/src/main/java/com/example/finalreport/theater.kt
@@ -19,15 +19,11 @@ import android.location.Address
 import android.location.Geocoder
 import java.io.IOException
 
-
-
 class theater : AppCompatActivity() {
     private lateinit var myBind:ActivityTheaterBinding
     private val targetUrl = "https://data.ntpc.gov.tw/api/datasets/61C99F42-8A90-4ADC-9C40-BA9E0EA097AA/json?page=0&size=1000"
     private var getString = ""
     private var getString2 = ""
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         myBind = ActivityTheaterBinding.inflate(layoutInflater)
@@ -48,15 +44,14 @@ class theater : AppCompatActivity() {
                         Log.d("myTag", "$getString")
 
                         val jsonData = JSONArray(getString)
-                        if (jsonData.length() > 1) {
-                            val jO = jsonData.getJSONObject(1)
+                        val resultStringBuilder = StringBuilder()
+
+                        for (i in 0 until jsonData.length()) {
+                            val jO = jsonData.getJSONObject(i)
                             val jname = jO.getString("name")
                             val jaddress = jO.getString("address")
                             val jphone = jO.getString("tel")
 
-                            getString = "電影院 : $jname\n" +
-                                    "Telephone :$jphone\n" +
-                                    "address : $jaddress"
 
 
                             // 地址轉經緯
@@ -77,16 +72,24 @@ class theater : AppCompatActivity() {
                                     Log.d("myTag", "Unable to convert address to LatLng.")
                                 }
                             }
-                        } else {
-                            Log.d("myTag", "Error: JSON array ")
+
+                            val theaterInfo = "电影院 : $jname\n" +
+                                    "Telephone :$jphone\n" +
+                                    "address : $jaddress\n"+
+                                    "經緯度:\n$getString2"
+
+                            resultStringBuilder.append(theaterInfo)
+                            //
+                            withContext(Dispatchers.Main) {
+                                myBind.textView.text = resultStringBuilder.toString()
+                                Log.d("myTag", "$theaterInfo ")
+                            }
                         }
                     }
                 } catch (e: IOException) {
                     Log.e("myTag", "Error: ${e.toString()}")
-                }
-                runOnUiThread {
-                    myBind.textView.text = getString + getString2
-                    Log.d("myTag", "g1+g2 ")
+                } catch (e: JSONException) {
+                    Log.e("myTag", "Error: ${e.toString()}")
                 }
             }
         }
